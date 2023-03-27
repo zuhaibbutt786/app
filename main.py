0172b296b3dc5cdaed975284ee84afa25a8be733
zuhaibbutt786 <67970420+zuhaibbutt786@users.noreply.github.com>
2023-03-27 15:02:10 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.write("hello world")
+st.button('Click me')
+st.checkbox('I agree')
+st.radio('Pick one', ['cats', 'dogs'])
+st.selectbox('Pick one', ['cats', 'dogs'])
+st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
+st.slider('Pick a number', 0, 100)
+st.select_slider('Pick a size', ['S', 'M', 'L'])
+st.text_input('First name')
+st.number_input('Pick a number', 0, 10)
+st.text_area('Text to translate')
+st.date_input('Your birthday')
+st.time_input('Meeting time')
+st.file_uploader('Upload a CSV')
+st.camera_input("Take a picture")
+st.color_picker('Pick a color')
